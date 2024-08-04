63415496b37abdcc4e87ed323725e78c2763b022
Daniil Savitskii <danildaniel121@gmail.com>
2024-08-04 20:02:01 +0400

    removed connector

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import { createClient } from "./lib/server.js";
 import { getConfig } from "./config/config.js";
 import { getDBClient } from "./db/client.js";
-import { connector } from "./lib/service-connector/connector.js";
 import { telegramChannelUpdateListner } from "./lib/telegram-channel-listener/listner.js";
 import { createLogger } from "./logger/logger.js";
 import { keepAlive } from "./lib/auth/keepAlive.js";
@@ -10,10 +9,11 @@ const logger = createLogger();
 
 const main = async () => {
   const config = getConfig();
+
   const db = await getDBClient(config, logger);
   const client = await createClient(db, config, logger);
 
-  connector(client, telegramChannelUpdateListner, [db, logger]);
+  telegramChannelUpdateListner(client, db, logger);
 
   keepAlive(client, logger);
 };
